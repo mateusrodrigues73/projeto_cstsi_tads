e36eede9b9047db20d1f2ca476d6e49b87ab2335
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-08-18 10:26:05 -0300

    feat: adicionar estrutura inicial do projeto

---
+++ src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/ProjetoCstsiTadsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.projeto_cstsi_tads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCstsiTadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoCstsiTadsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/ProjetoCstsiTadsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.projeto_cstsi_tads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCstsiTadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
