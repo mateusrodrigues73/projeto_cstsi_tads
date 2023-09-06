75d2df3f202ae8dfab7cc0efcab8df6a7d6860e9
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-09-06 11:38:15 -0300

    chore: adicionar relação com classeRole, implementar UserDetails

--- src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/participante/Participante.java
+++ src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/participante/Participante.java
@@ -3,6 +3,10 @@ package br.edu.ifsul.cstsi.projeto_cstsi_tads.api.participante;
 import br.edu.ifsul.cstsi.projeto_cstsi_tads.api.lance.Lance;
 import javax.persistence.*;
 import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class Participante {
+public class Participante implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "id")
@@ -26,4 +30,44 @@ public class Participante {
     private String telefone;
     @OneToMany(mappedBy = "participante")
     private Collection<Lance> lances;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "participantes_roles",
+            joinColumns = @JoinColumn(name = "participante_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
