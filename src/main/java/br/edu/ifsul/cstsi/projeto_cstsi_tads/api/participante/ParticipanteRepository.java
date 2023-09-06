87d3b169d7cb33a7477b07702b823281127cf697
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-09-06 11:36:50 -0300

    feat: adicionar método findByLogin

--- src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/participante/ParticipanteRepository.java
+++ src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/participante/ParticipanteRepository.java
@@ -1,9 +1,7 @@
 package br.edu.ifsul.cstsi.projeto_cstsi_tads.api.participante;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
 
-//import java.util.Date;
-//import java.util.List;
-
-public interface ParticipanteRepository extends JpaRepository<Participante, Long>{}
+public interface ParticipanteRepository extends JpaRepository<Participante, Long>{
+    Participante findByLogin(String login);
+}
