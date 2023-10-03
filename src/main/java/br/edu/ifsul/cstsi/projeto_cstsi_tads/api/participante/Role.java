7b2cc72aff030dcab25a222bef3caa390c59d07b
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-10-03 12:44:44 -0300

    feat: adicionar construtores na classe Role

--- src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/participante/Role.java
+++ src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/participante/Role.java
@@ -1,6 +1,7 @@
 package br.edu.ifsul.cstsi.projeto_cstsi_tads.api.participante;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.Entity;
@@ -10,11 +11,16 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+
+    public Role(Long id) {
+        this.id = id;
+    }
 
     @Override
     public String getAuthority() {
