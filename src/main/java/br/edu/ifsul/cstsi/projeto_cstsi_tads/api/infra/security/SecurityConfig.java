65f6db9a6844898fb65c46c69040dae5f72ef219
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-10-03 12:46:26 -0300

    feat: alterar método de login com Token JWT

--- src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/infra/security/SecurityConfig.java
+++ src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/infra/security/SecurityConfig.java
@@ -1,16 +1,20 @@
 package br.edu.ifsul.cstsi.projeto_cstsi_tads.api.infra.security;
 
+import br.edu.ifsul.cstsi.projeto_cstsi_tads.api.infra.security.jwt.JwtAuthenticationFilter;
+import br.edu.ifsul.cstsi.projeto_cstsi_tads.api.infra.security.jwt.JwtAuthorizationFilter;
+import br.edu.ifsul.cstsi.projeto_cstsi_tads.api.infra.security.jwt.handler.AccessDeniedHandler;
+import br.edu.ifsul.cstsi.projeto_cstsi_tads.api.infra.security.jwt.handler.UnauthorizedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,6 +26,10 @@ public class SecurityConfig {
     @Autowired
     @Qualifier("userDetailsService")
     private UserDetailsService userDetailsService;
+    @Autowired
+    private UnauthorizedHandler unauthorizedHandler;
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -35,16 +43,39 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //Basic Authentication
+        //Configuração JWT Authentication
+        final AuthenticationManager authManager = authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
         http
-            .csrf().disable()
-            .authorizeHttpRequests()
-            .antMatchers(HttpMethod.POST, "/api/v1/participantes/new").permitAll() //porém, esse path é exceção
+            .authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/api/v1/login").permitAll()
+            .antMatchers(HttpMethod.POST, "/api/v1/participantes/new").permitAll()
             .anyRequest().authenticated()
             .and()
-            .httpBasic(Customizer.withDefaults())
-            .userDetailsService(userDetailsService);
+            .csrf().disable()
+            .addFilter(new JwtAuthenticationFilter(authManager))
+            .addFilter(new JwtAuthorizationFilter(authManager, userDetailsService))
+            .exceptionHandling()
+            .accessDeniedHandler(accessDeniedHandler)
+            .authenticationEntryPoint(unauthorizedHandler)
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        //Basic Authentication
+//        http
+//                .csrf().disable()
+//                .authorizeHttpRequests()
+//                .antMatchers(HttpMethod.POST, "/api/v1/participantes/new").permitAll() //porém, esse path é exceção
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic(Customizer.withDefaults())
+//                .userDetailsService(userDetailsService);
+//
+//        return http.build();
+//    }
 
         return http.build();
     }
+
 }
