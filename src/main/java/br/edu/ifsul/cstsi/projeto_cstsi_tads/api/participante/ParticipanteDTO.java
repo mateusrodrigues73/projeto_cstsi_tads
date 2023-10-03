1acdc22cedd988b262d22c12379e67e4971d25fc
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-10-03 12:43:22 -0300

    feat: adicionar token na classe DTO de participantes

--- src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/participante/ParticipanteDTO.java
+++ src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/participante/ParticipanteDTO.java
@@ -1,11 +1,16 @@
 package br.edu.ifsul.cstsi.projeto_cstsi_tads.api.participante;
 
-import br.edu.ifsul.cstsi.projeto_cstsi_tads.api.leilao.Leilao;
-import br.edu.ifsul.cstsi.projeto_cstsi_tads.api.leilao.LeilaoDTO;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ParticipanteDTO {
     private Long id;
     private String nome;
@@ -14,9 +19,27 @@ public class ParticipanteDTO {
     private String email;
     private String endereco;
     private String telefone;
+    private String token;
+
+    private List<String> roles;
 
     public static ParticipanteDTO create (Participante p) {
         ModelMapper modelMapper = new ModelMapper();
-        return modelMapper.map(p, ParticipanteDTO.class);
+        ParticipanteDTO dto = modelMapper.map(p, ParticipanteDTO.class);
+        dto.roles = p.getRoles().stream()
+                .map(Role::getNome)
+                .collect(Collectors.toList());
+        return dto;
+    }
+
+    public static ParticipanteDTO create(Participante p, String token) {
+        ParticipanteDTO dto = create(p);
+        dto.token = token;
+        return dto;
+    }
+
+    public String toJson() throws JsonProcessingException {
+        ObjectMapper m = new ObjectMapper();
+        return m.writeValueAsString(this);
     }
 }
