aa0a9d6a7ee5e32147b55501377f2e6304a02ab2
Aluno <aluno@example.com>
2023-10-11 15:48:32 -0300

    "feat: adicionar classe de test leilao service"

---
+++ src/test/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/leilao/LeilaoServiceTest.java
@@ -0,0 +1,29 @@
+package br.edu.ifsul.cstsi.projeto_cstsi_tads.api.leilao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class LeilaoServiceTest {
+
+    @Test
+    void getLeiloes() {
+    }
+
+    @Test
+    void getLeilaoById() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
