46dd7edf82e2178bc9a76fac7b8c4809e032fdb9
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-10-25 11:17:26 -0300

    feat: testes controller leilao

---
+++ src/test/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/leilao/LeilaoControllerTest.java
@@ -0,0 +1,155 @@
+package br.edu.ifsul.cstsi.projeto_cstsi_tads.api.leilao;
+
+import br.edu.ifsul.cstsi.projeto_cstsi_tads.ProjetoCstsiTadsApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+import java.util.Objects;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ProjetoCstsiTadsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LeilaoControllerTest extends BaseAPITest{
+
+    //Métodos utilitários
+    private ResponseEntity<LeilaoDTO> getLeilao(String url) {
+        return get(url, LeilaoDTO.class);
+    }
+
+    private ResponseEntity<List<LeilaoDTO>> getLeiloes(String url) {
+        HttpHeaders headers = getHeaders();
+        return rest.exchange(
+            url,
+            HttpMethod.GET,
+            new HttpEntity<>(headers),
+            new ParameterizedTypeReference<List<LeilaoDTO>>() {}
+        );
+    }
+
+    // Testes
+    @Test
+    public void selectAll() {
+        List<LeilaoDTO> leiloes = getLeiloes("/api/v1/leiloes").getBody();
+        assertNotNull(leiloes);
+        assertEquals(5, leiloes.size());
+        leiloes = getLeiloes("/api/v1/leiloes").getBody();
+        assertNotNull(leiloes);
+        assertEquals(5, leiloes.size());
+    }
+
+    @Test
+    public void selectById() {
+        assertNotNull(getLeilao("/api/v1/leiloes/1"));
+        assertNotNull(getLeilao("/api/v1/leiloes/2"));
+        assertNotNull(getLeilao("/api/v1/leiloes/3"));
+        assertNotNull(getLeilao("/api/v1/leiloes/4"));
+        assertNotNull(getLeilao("/api/v1/leiloes/5"));
+        assertEquals(HttpStatus.NOT_FOUND, getLeilao("/api/v1/leiloes/1000").getStatusCode());
+    }
+
+    // TODO: consertar erro da data
+
+    @Test
+    public void testInsert() {
+        // Cria objetos Date e Time para as datas e horas desejadas
+        Date dataInicio = Date.valueOf("2023-10-21");
+        Date dataFinal = Date.valueOf("2023-10-21");
+        Time horaInicio = Time.valueOf("12:00:00");
+        Time horaFinal = Time.valueOf("16:00:00");
+
+        //cria o leilao para teste
+        Leilao leilao = new Leilao();
+        leilao.setDataInicio(dataInicio);
+        leilao.setDataFinal(dataFinal);
+        leilao.setHoraInicio(horaInicio);
+        leilao.setHoraFinal(horaFinal);
+
+        // Insert
+        ResponseEntity<Object> response = post("/api/v1/leiloes", leilao, null);
+        System.out.println("response: ");
+        System.out.println(response);
+
+        // Verifica se criou
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+        // Busca o objeto
+        String location = Objects.requireNonNull(response.getHeaders().get("location")).get(0);
+        LeilaoDTO l = getLeilao(location).getBody();
+
+        assertNotNull(l);
+        assertEquals(dataInicio, l.getDataInicio());
+
+        // Deleta o objeto
+        delete(location, null);
+
+        // Verificar se deletou
+        assertEquals(HttpStatus.NOT_FOUND, getLeilao(location).getStatusCode());
+    }
+
+    @Test
+    public void testUpdate() {
+        // Cria objetos Date e Time para as datas e horas desejadas
+        Date dataInicio = Date.valueOf("2023-10-21");
+        Date dataFinal = Date.valueOf("2023-10-21");
+        Time horaInicio = Time.valueOf("12:00:00");
+        Time horaFinal = Time.valueOf("16:00:00");
+
+        //cria o leilao para teste
+        Leilao leilao = new Leilao();
+        leilao.setDataInicio(dataInicio);
+        leilao.setDataFinal(dataFinal);
+        leilao.setHoraInicio(horaInicio);
+        leilao.setHoraFinal(horaFinal);
+
+        // Insert
+        ResponseEntity<Object> response = post("/api/v1/leiloes", leilao, null);
+        System.out.println("response: ");
+        System.out.println(response);
+
+        // Verifica se criou
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+        // Busca o objeto
+        String location = Objects.requireNonNull(response.getHeaders().get("location")).get(0);
+        LeilaoDTO l = getLeilao(location).getBody();
+
+        assertNotNull(l);
+        assertEquals(dataInicio, l.getDataInicio());
+
+        //depois altera seu valor
+        Leilao la = Leilao.create(l);
+        la.setDataInicio(Date.valueOf("2023-10-11"));
+
+        // Update
+        response = put("/api/v1/leiloes/" + l.getId(), la, null);
+        System.out.println(response);
+        assertEquals(Date.valueOf("2023-10-11"), la.getDataInicio());
+
+        // Deleta o objeto
+        delete(location, null);
+
+        // Verifica se deletou
+        assertEquals(HttpStatus.NOT_FOUND, getLeilao(location).getStatusCode());
+    }
+
+    @Test
+    public void testDelete() {
+        this.testInsert();
+    }
+
+    @Test
+    public void testGetNotFound() {
+        ResponseEntity response = getLeilao("/api/v1/leiloes/1100");
+        assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
+    }
+}
+
