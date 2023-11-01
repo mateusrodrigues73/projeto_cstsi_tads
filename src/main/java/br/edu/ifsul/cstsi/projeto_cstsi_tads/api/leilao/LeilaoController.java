993794122fa561e84173c52731d9714768b10dc2
Aluno <aluno@example.com>
2023-11-01 14:17:52 -0300

    feat: documentar controller de leilões

--- src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/leilao/LeilaoController.java
+++ src/main/java/br/edu/ifsul/cstsi/projeto_cstsi_tads/api/leilao/LeilaoController.java
@@ -1,5 +1,7 @@
 package br.edu.ifsul.cstsi.projeto_cstsi_tads.api.leilao;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -11,18 +13,20 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("api/v1/leiloes")
-
+@Api(value = "Leiloẽs")
 public class LeilaoController {
     @Autowired
     private LeilaoService service;
 
     @GetMapping
+    @ApiOperation(value = "Retorna todos os leilões cadastrados.")
     public ResponseEntity<List<LeilaoDTO>> selectAll() {
         List<LeilaoDTO> leiloes = service.getLeiloes();
         return ResponseEntity.ok(leiloes);
     }
 
     @GetMapping("{id}")
+    @ApiOperation(value = "Retorna um leilão pelo campo identificador.")
     public ResponseEntity<LeilaoDTO> selectById(@PathVariable("id") Long id) {
         LeilaoDTO l = service.getLeilaoById(id);
         return l != null ? ResponseEntity.ok(l) : ResponseEntity.notFound().build();
@@ -30,6 +34,7 @@ public class LeilaoController {
 
     @Secured({"ROLE_ADMIN"})
     @PostMapping
+    @ApiOperation(value = "Insere um novo leilão.")
     public ResponseEntity<String> insert(@RequestBody Leilao leilao) {
         LeilaoDTO l = service.insert(leilao);
         URI location = getUri(l.getId());
@@ -37,6 +42,7 @@ public class LeilaoController {
     }
 
     @PutMapping("{id}")
+    @ApiOperation(value = "Atualiza um leilao existente.")
     public ResponseEntity<LeilaoDTO> update(@PathVariable("id") Long id, @RequestBody Leilao leilao) {
         leilao.setId(id);
         LeilaoDTO l = service.update(leilao, id);
@@ -45,6 +51,7 @@ public class LeilaoController {
     }
 
     @DeleteMapping("{id}")
+    @ApiOperation(value = "Deleta um leilão.")
     public ResponseEntity<String> delete(@PathVariable("id") Long id) {
         return service.delete(id) ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
